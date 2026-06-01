362f4422b60b84284ffdb6abe01b5100e22c8b82
adishahirushan <adishahirushan@gmail.com>
2026-06-01 14:37:44 +0530

    do given tasks bgcolorimage and add new button with action

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function showMessage() {
     document.getElementById("message").innerHTML = "CI/CD Pipeline Working!";
 }
+function showMyName() {
+    document.getElementById("myName").innerHTML = "My name is Adisha Hirushan";
+}
