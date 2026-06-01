f8f83f37a71a2d41235e74e30c250fba789f8957
adishahirushan <adishahirushan@gmail.com>
2026-06-01 14:22:10 +0530

    deploy.yaml and fw changes on other files

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function showMessage() {
-    document.getElementById("message").innerHTML = "CI?CD Pipeline Working!";
+    document.getElementById("message").innerHTML = "CI/CD Pipeline Working!";
 }
